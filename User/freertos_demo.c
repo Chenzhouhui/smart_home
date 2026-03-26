f102617152bb3cf81d438b543ae5e194556e31bc
Chenzhouhui <2691905230@qq.com>
2026-03-26 23:39:53 +0800

    add lcd

--- User/freertos_demo.c
+++ User/freertos_demo.c
@@ -21,7 +21,7 @@ void start_task(void *pvParameters);        /* 任务函数 */
 /* TASK1 任务 配置
  * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
  */
-#define TASK1_PRIO      2                   /* 任务优先级 */
+#define TASK1_PRIO      3                   /* 任务优先级 */
 #define TASK1_STK_SIZE  128                 /* 任务堆栈大小 */
 TaskHandle_t            Task1Task_Handler;  /* 任务句柄 */
 void task1(void *pvParameters);             /* 任务函数 */
@@ -29,7 +29,7 @@ void task1(void *pvParameters);             /* 任务函数 */
 /* TASK2 任务 配置
  * 包括: 任务句柄 任务优先级 堆栈大小 创建任务
  */
-#define TASK2_PRIO      3                   /* 任务优先级 */
+#define TASK2_PRIO      2                   /* 任务优先级 */
 #define TASK2_STK_SIZE  128                 /* 任务堆栈大小 */
 TaskHandle_t            Task2Task_Handler;  /* 任务句柄 */
 void task2(void *pvParameters);             /* 任务函数 */
@@ -98,7 +98,7 @@ void task1(void *pvParameters)
 		LED_ON();
         vTaskDelay(200);
         LED_OFF();
-        vTaskDelay(300);                           /* 延时300ticks */
+        vTaskDelay(200);                           /* 延时300ticks */
   
     }
 }
@@ -137,6 +137,6 @@ void task2(void *pvParameters)
         LCD_ShowNum(62, 50, frame++, 5, 16);
 
    
-        vTaskDelay(500);                           /* 延时500ticks */
+//        vTaskDelay(500);                           /* 延时500ticks */
     }
 }
